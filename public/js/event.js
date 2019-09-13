7bf1d339981a4c9dcf80ebbd8e490630760c928e
Nguyễn Hữu Trường <38267209+huutruong2304@users.noreply.github.com>
2019-09-13 09:40:01 +0700

    thay doi giao dien

--- public/js/event.js
+++ public/js/event.js
@@ -26,6 +26,7 @@ const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', e => {
     e.preventDefault();
     messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
     const location = search.value;
     fetch('/weather?location=' + location).then(response => {
         response.json().then(data => {
